4f9d912adce6d11c038eb5d783e23a3faf77eb89
Visal <sanvisal009@gmail.com>
2023-08-05 22:05:20 +0700

    Add Button3

--- GIT_1/Form1.Designer.cs
+++ GIT_1/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.button2 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -66,11 +67,21 @@
             this.textBox2.Size = new System.Drawing.Size(330, 22);
             this.textBox2.TabIndex = 2;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(524, 271);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 61);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
@@ -89,7 +100,7 @@
 
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
-
+        private System.Windows.Forms.Button button3;
     }
 }
 
